89270c02094012d771c87c9010e740619d70a91f
aaditya chapagain <chapagain.aaditya@gmail.com>
2021-06-12 09:18:22 +0545

    solved code for minimum absolute difference

--- minimum_absolute_difference_array/main.py
+++ minimum_absolute_difference_array/main.py
@@ -1,6 +1,21 @@
-
 def minimumAbsoluteDifference(arr):
-    pass
+    arr.sort()
+    initial = arr[0]
+    later = 0
+    MAD = 999999999999999
+    for i in arr[1:]:
+        later = i
+        CMAD = abs(initial - later)
+        if CMAD == 0:
+            return 0
+        if CMAD < MAD:
+            MAD = CMAD
+
+        initial = later 
+    
+    return MAD
+
 
 if __name__=='__main__':
-    minimumAbsoluteDifference([-59, -36, -13, 1, -53, -92, -2, -96, -54, 75])
+    val = minimumAbsoluteDifference([-59, -36, -13, 1, -53, -92, -2, -96, -54, 75])
+    print("minimum absolute difference: ", val)
