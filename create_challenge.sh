d2473c0d2739c002a2a9a0ee7265fac4003213ef
aaditya chapagain <chapagain.aaditya@gmail.com>
2021-07-01 09:24:44 +0545

    include go file and solution.md in create_challange script

--- create_challenge.sh
+++ create_challenge.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
 projdir=$1
 
 mkdir $projdir
 touch "${projdir}/readme.md"
 touch "${projdir}/__init__.py"
 touch "${projdir}/main.py"
+touch "${projdir}/main.go"
+touch "${projdir}/solution.md"
+
