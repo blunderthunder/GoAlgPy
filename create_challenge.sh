e638a3b572ad1db6a5b4a247a5c3d5a1856a4afd
aaditya chapagain <aadityachapagain101@gmail.com>
2022-04-16 15:05:27 +0545

    rust project

--- create_challenge.sh
+++ create_challenge.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
+
+create_proj_dir(){
+    touch "${1}/readme.md"
+    touch "${1}/__init__.py"
+    touch "${1}/main.py"
+    touch "${1}/main.go"
+    touch "${1}/solution.md"
+}
+
+create_rust_proj_dir(){
+    mkdir "${1}/tests"
+    touch "${1}/tests/${1}_test.rs"
+}
+
 projdir=$1
 
 echo $projdir > .activechallange
 
-mkdir $projdir
-touch "${projdir}/readme.md"
-touch "${projdir}/__init__.py"
-touch "${projdir}/main.py"
-touch "${projdir}/main.go"
-touch "${projdir}/solution.md"
+# create the rust project
+cargo new --lib $projdir
 
+if [ $? -eq 0 ]; then
+    create_proj_dir $projdir
+    create_rust_proj_dir $projdir
+else
+    create_proj_dir $projdir
+fi
+
+echo "Successfully created project ${projdir}"
