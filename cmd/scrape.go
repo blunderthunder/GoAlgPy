04deafeae8cdd002507f40afacf8d04d13683b34
Aaditya chapagain <chapagain.aaditya@gmail.com>
2022-09-07 20:39:15 +1000

    scrape cli command to scrape problem description from leetcode

--- cmd/scrape.go
+++ cmd/scrape.go
@@ -1,8 +1,12 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"strings"
 
+	"github.com/chromedp/chromedp"
 	"github.com/spf13/cobra"
 )
 
@@ -11,7 +15,23 @@ var scrapeCmd = &cobra.Command{
 	Short: "scrape LeetCode challenge Description",
 	Long:  "This command will scrape the innerHTML of description section of leetcode and save it as Markdown file.",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Scraping the file \nScraped Thank you for your patience.")
+		selector := "#app > div > div.main__2_tD > div.content__3fR6 > div > div.side-tools-wrapper__1TS9 > div > div.css-1gd46d6-Container.e5i1odf0 > div.css-jtoecv > div > div.tab-pane__ncJk.css-1eusa4c-TabContent.e5i1odf5 > div > div.content__u3I1.question-content__JfgR"
+
+		ctx, cancel := chromedp.NewContext(context.Background())
+		defer cancel()
+
+		// run task and srape
+		var res string
+		err := chromedp.Run(ctx,
+			chromedp.Navigate(args[0]),
+			chromedp.Text(selector, &res, chromedp.NodeVisible),
+		)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(strings.TrimSpace(res))
 	},
 }
 
--- go.mod
+++ go.mod
@@ -3,12 +3,14 @@ module github.com/aadityachapagain/GoAlgPy
 go 1.16
 
 require (
+	github.com/chromedp/cdproto v0.0.0-20220901095120-1a01299a2163 // indirect
+	github.com/chromedp/chromedp v0.8.5
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
 	github.com/spf13/afero v1.9.2 // indirect
 	github.com/spf13/cobra v1.5.0
 	github.com/spf13/viper v1.12.0
 	github.com/subosito/gotenv v1.4.1 // indirect
-	golang.org/x/sys v0.0.0-20220829200755-d48e67d00261 // indirect
+	golang.org/x/sys v0.0.0-20220907062415-87db552b00fd // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 )
