1af4e0694edaef46263bd8663c95d1d0ca756d60
Aaditya chapagain <chapagain.aaditya@gmail.com>
2022-09-21 00:25:23 +1000

    all possible full binary tree using rust

--- all_possible_full_binary_trees/src/lib.rs
+++ all_possible_full_binary_trees/src/lib.rs
@@ -16,6 +16,7 @@ impl TreeNode {
     }
   }
 }
+use std::collections::HashMap;
 use std::rc::Rc;
 use std::cell::RefCell;
 
@@ -23,6 +24,31 @@ pub struct Solution;
 
 impl Solution {
     pub fn all_possible_fbt(n: i32) -> Vec<Option<Rc<RefCell<TreeNode>>>> {
+      let mut cache = HashMap::from([
+        (0, vec![]),
+        (1, vec![Some(Rc::new(RefCell::new(TreeNode::new(0))))])
+      ]);
+
+      fn backtrack(n: i32, hash: &mut HashMap<i32, Vec<Option<Rc<RefCell<TreeNode>>>>>) -> Vec<Option<Rc<RefCell<TreeNode>>>> {
         
+        if hash.contains_key(&n){
+          return hash[&n].clone()
+        }
+
+        let mut res: Vec<Option<Rc<RefCell<TreeNode>>>> = Vec::new();
+
+        for l in 0..n{
+          let r = n - 1 - l;
+          for lefttree in backtrack(l, hash){
+            for righttree in backtrack(r, hash){
+              
+              res.push(Some(Rc::new(RefCell::new(TreeNode{val: 0, left: lefttree.clone(), right: righttree}))))
+            }
+          }
+        }
+        hash.insert(n, res.clone());
+        return res
+      }
+      backtrack(n, &mut cache)
     }
 }
--- all_possible_full_binary_trees/src/main.rs
+++ all_possible_full_binary_trees/src/main.rs
@@ -1,3 +1,5 @@
+
+use all_possible_full_binary_trees::Solution;
 fn main() {
-    println!("Hello, world!");
+    println!("solution of n = 7 is : {:?}", Solution::all_possible_fbt(7))
 }
