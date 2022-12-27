e3d11baefc0f125c1ed65f3d5699a3d7118c86aa
Aaditya chapagain <chapagain.aaditya@gmail.com>
2022-12-27 20:30:14 +1100

    fix | scraper

--- cmd/neochallange.go
+++ cmd/neochallange.go
@@ -14,7 +14,7 @@ import (
 )
 
 func scrapeDesc(url string) string {
-	selector := "#app > div > div.main__2_tD > div.content__3fR6 > div > div.side-tools-wrapper__1TS9 > div > div.css-1gd46d6-Container.e5i1odf0 > div.css-jtoecv > div > div.tab-pane__ncJk.css-1eusa4c-TabContent.e5i1odf5 > div > div.content__u3I1.question-content__JfgR > div"
+	selector := "#qd-content > div.h-full.flex-col.ssg__qd-splitter-primary-w > div > div > div > div.flex.h-full.w-full.overflow-y-auto > div > div > div:nth-child(3) > div"
 
 	ctx, cancel := chromedp.NewContext(context.Background())
 	defer cancel()
--- cmd/scrape.go
+++ cmd/scrape.go
@@ -16,7 +16,7 @@ var scrapeCmd = &cobra.Command{
 	Long:  "This command will scrape the innerHTML of description section of leetcode and save it as Markdown file.",
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		selector := "#app > div > div.main__2_tD > div.content__3fR6 > div > div.side-tools-wrapper__1TS9 > div > div.css-1gd46d6-Container.e5i1odf0 > div.css-jtoecv > div > div.tab-pane__ncJk.css-1eusa4c-TabContent.e5i1odf5 > div > div.content__u3I1.question-content__JfgR"
+		selector := "#qd-content > div.h-full.flex-col.ssg__qd-splitter-primary-w > div > div > div > div.flex.h-full.w-full.overflow-y-auto > div > div > div:nth-child(3) > div"
 
 		ctx, cancel := chromedp.NewContext(context.Background())
 		defer cancel()
